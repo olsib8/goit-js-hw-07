251fabd4738e1a1da6bb62557eb0911d9ff4de05
olsib8 <alsib8@gmail.com>
2023-02-20 20:03:54 +0200

    task-2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,22 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const container = document.querySelector('.gallery');
+const arrayOfGallaryElements = createElementsOfGallery(galleryItems);
+
+function createElementsOfGallery(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `<li class="gallery__item">
+  <a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" title="${description}"/>
+</a>
+</li>`;
+    })
+    .join('');
+}
+container.insertAdjacentHTML('beforeend', arrayOfGallaryElements);
+
+let gallery = new SimpleLightbox('.gallery a', {
+  captionDelay: 250,
+});
